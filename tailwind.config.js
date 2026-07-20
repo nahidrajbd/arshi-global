8249fc9ff05ede6017a5ce58e7174377a944ec14
Nahid Hossain <nahid.rajbd@yahoo.com>
2026-07-20 14:56:52 +0600

    Branding update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./app/**/*.{js,jsx}'],
-  theme: { extend: { colors: { arshi: { red: '#B42A1D', wine: '#90281B', orange: '#FF6638', peach: '#ED9279', ink: '#304132', sage: '#4F7E6A', mist: '#FAFAFA' } }, fontFamily: { sans: ['var(--font-sans)'] } } },
+  theme: { extend: { colors: { arshi: { red: '#046f81', wine: '#035a6a', orange: '#05a0ba', peach: '#7dd8e6', ink: '#0d2d35', sage: '#1a6070', mist: '#f0fafc' } }, fontFamily: { sans: ['var(--font-sans)'] } } },
   plugins: []
 };
